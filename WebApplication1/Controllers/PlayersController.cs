b71bab04112cf9b32e50b38747bf759db07cc206
mygit@23 <naveenmadhesan23@gmail.com>
2024-02-20 14:05:08 +0530

    Add project files.

---
+++ WebApplication1/Controllers/PlayersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayersController : ControllerBase
+    {
+        List<Players> players = new List<Players> {
+    new Players(){Id=1,Name="msd",Avg=20,Team="csk"},
+    new Players(){Id=2,Name="virat",Avg=45,Team="rcb"},
+    new Players(){Id=3,Name="rohit",Avg=42,Team="mi"},
+    new Players(){Id=4,Name="sachin",Avg=48,Team="mi"},
+    new Players(){Id=5,Name="abd",Avg=39,Team="rcb"}
+};
+        [HttpGet]
+        public ActionResult<IEnumerable<Players>> Get() { return players; }
+    }
+}
---
+++ WebApplication1/Models/Players.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class Players
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Avg { get; set; }
+        public string Team { get; set; }
+    }
+}
